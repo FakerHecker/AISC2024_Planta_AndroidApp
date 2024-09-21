1e0a56bca7441966b4f0dcc6aaef0916dfba538b
huytt268 <tuanhuytruong35@gmail.com>
2024-09-21 17:52:57 +0700

    Add News Section to Home Screen

--- app/src/main/java/com/example/aisc2024_planta_androidapp/home/HomeScreen.kt
+++ app/src/main/java/com/example/aisc2024_planta_androidapp/home/HomeScreen.kt
@@ -1,11 +1,22 @@
 package com.example.aisc2024_planta_androidapp.home
+
+import android.util.Log
+import android.widget.HorizontalScrollView
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -17,7 +28,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -40,13 +54,12 @@ fun HomeScreen() {
         // Header
         Row(
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             // Icon profile
             Image(
                 painter = painterResource(id = R.drawable.container_profile),
                 contentDescription = "User Profile",
-                modifier = Modifier
-                    .size(40.dp)
+                modifier = Modifier.size(40.dp)
             )
             Spacer(modifier = Modifier.size(10.dp))
             // Text Header
@@ -88,12 +101,60 @@ fun HomeScreen() {
         TaskList()
 
         // News Section
-        Text(
-            text = "Tin tức mới",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Tin tức mới",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF005200),
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable(
+                        onClick = {
+                        },
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() })
+                    .background(Color.Transparent)
+                    .offset(x = (16).dp),
+            ) {
+                Text(
+                    text = "Xem tất cả",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF059710)
+                )
+            }
+//            TextButton(
+//                onClick = {},
+//                modifier = Modifier
+//                    .background(Color.Transparent)
+//                    .offset(x = (16).dp)
+//                    .indication(
+//                        interactionSource = remember { MutableInteractionSource() },
+//                        indication = null
+//                    ),
+//                colors = ButtonDefaults.textButtonColors(
+//                    containerColor = Color.Transparent,
+//                    contentColor = Color(0xFF059710)
+//                ),
+//                shape = RectangleShape,
+//                contentPadding = PaddingValues(15.dp, 12.dp),
+//            ) {
+//                Text(
+//                    text = "Xem tất cả",
+//                    fontSize = 20.sp,
+//                    fontWeight = FontWeight.Bold
+//                )
+//            }
+        }
+        Spacer(modifier = Modifier.padding(2.dp))
         NewsSection()
 
         // Recommendations
@@ -110,34 +171,32 @@ fun HomeScreen() {
 @Composable
 fun WeatherInfo() {
     Card(
-        modifier = Modifier
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         colors = CardDefaults.cardColors(containerColor = Color(0xFFE8F5E9))
-    )
-    {
+    ) {
         Column() {
             Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(16.dp)
+                verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)
             ) {
                 // Left side: Weather information
                 Column(
                     modifier = Modifier.weight(1f)
                 ) {
-                    Row()
-                    {
-                        Icon(imageVector = Icons.Default.LocationOn, 
-                            contentDescription = "Location", 
-                            modifier = Modifier.size(16.dp))
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Default.LocationOn,
+                            contentDescription = "Location",
+                            modifier = Modifier.size(16.dp)
+                        )
                         Spacer(modifier = Modifier.size(3.dp))
                         ClickableText(
                             text = AnnotatedString(
-                                text = "Thành phố Hồ Chí Minh",
-                                spanStyle = SpanStyle(
+                                text = "Thành phố Hồ Chí Minh", spanStyle = SpanStyle(
                                     fontSize = 16.sp,
                                     fontWeight = FontWeight.Bold,
                                 )
-                            )) {
+                            )
+                        ) {
                             // Can click to call API location
                         }
                     }
@@ -149,9 +208,7 @@ fun WeatherInfo() {
                         color = Color.Black
                     )
                     Text(
-                        text = "Nắng đẹp",
-                        fontSize = 20.sp,
-                        color = Color.Black
+                        text = "Nắng đẹp", fontSize = 20.sp, color = Color.Black
                     )
 
                     // Grid-like structure for weather details
@@ -174,8 +231,12 @@ fun WeatherInfo() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    WeatherDetailItem(iconRes = R.drawable.icon_uv, label = "Chỉ số UV", value = "Trung bình")
-                    WeatherDetailItem(iconRes = R.drawable.icon_uv, label = "Tốc độ gió", value = "16km/h")
+                    WeatherDetailItem(
+                        iconRes = R.drawable.icon_uv, label = "Chỉ số UV", value = "Trung bình"
+                    )
+                    WeatherDetailItem(
+                        iconRes = R.drawable.icon_uv, label = "Tốc độ gió", value = "16km/h"
+                    )
                 }
                 // Second Row
                 Row(
@@ -183,7 +244,9 @@ fun WeatherInfo() {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     WeatherDetailItem(iconRes = R.drawable.icon_uv, label = "Độ ẩm", value = "58%")
-                    WeatherDetailItem(iconRes = R.drawable.icon_uv, label = "Khả năng mưa", value = "14%")
+                    WeatherDetailItem(
+                        iconRes = R.drawable.icon_uv, label = "Khả năng mưa", value = "14%"
+                    )
                 }
             }
         }
@@ -193,19 +256,19 @@ fun WeatherInfo() {
 
 @Composable
 fun WeatherDetailItem(iconRes: Int, label: String, value: String) {
-    Box(modifier = Modifier
-        .size(width = 160.dp, height = 60.dp)){
+    Box(
+        modifier = Modifier.size(width = 160.dp, height = 60.dp)
+    ) {
         Row(
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier.padding(start = 12.dp)
-        ){
+            horizontalArrangement = Arrangement.Start, modifier = Modifier.padding(start = 12.dp)
+        ) {
             Image(
                 painter = painterResource(id = iconRes),
                 contentDescription = label,
                 modifier = Modifier.size(40.dp) // Adjust icon size
             )
             Spacer(modifier = Modifier.size(2.dp))
-            Column (){
+            Column() {
                 Text(text = label, fontSize = 12.sp, color = Color.Gray)
                 Text(text = value, fontSize = 14.sp, fontWeight = FontWeight.Bold)
             }
@@ -240,16 +303,95 @@ fun TaskItem(name: String, time: String, action: String) {
 
 @Composable
 fun NewsSection() {
-    Column {
+    val scrollState = rememberScrollState()
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(14.dp),
+        modifier = Modifier.horizontalScroll(scrollState)
+    ) {
         // Example news items
-        NewsItem("Cách chăm sóc cây cảnh trong nhà")
-        NewsItem("Cách chăm sóc cây đuôi công")
+        NewsItem("post 1")
+        NewsItem("post 2")
+        NewsItem("post 3")
     }
 }
 
 @Composable
 fun NewsItem(title: String) {
-    Text(text = title, modifier = Modifier.padding(vertical = 4.dp))
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        modifier = Modifier
+            .clickable(onClick = {})
+            .width(220.dp)
+            .height(248.dp)
+            .clip(RoundedCornerShape(6.dp)),
+        border = BorderStroke(
+            1.66.dp, Color(0xFFCAE0CD)
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            //plant iamge
+            Image(
+                painter = painterResource(id = R.drawable.post_image),
+                contentDescription = "Post Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(95.dp)
+                    .clip(RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            //title & tag
+            Column(
+            ) {
+                Text(
+                    text = "Cách chăm sóc cây cảnh trong nhà",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF005200)
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(0.dp)
+                ) {
+                    Text(
+                        text = "Chăm sóc cây · Mẹo vặt",
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Color(0xFF005200)
+                    )
+                    Text(
+                        text = "15/08/2024 · 3 phút đọc",
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Color(0xFF005200)
+                    )
+                }
+            }
+            //author
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.author_image),
+                    contentDescription = "Tác giả",
+                    modifier = Modifier.size(25.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Planta",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF005200)
+                )
+            }
+        }
+
+    }
 }
 
 @Composable
